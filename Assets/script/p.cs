3e5707fe73dd1e298e00e6adf24e30316bcf05b8
jojo <kkokk88@naver.com>
2023-04-04 23:26:39 +0900

    2023.04.04

--- Assets/script/p.cs
+++ Assets/script/p.cs
@@ -17,9 +17,26 @@ public class p : MonoBehaviour
 
     void Update()
     {
+        Rotation();
+        Revolution();
+    }
+
+    void Rotation()
+    {
         transform.Rotate(new Vector3(0, 20, 0) * speed * Time.deltaTime);
         timeCounter += Time.deltaTime;
-        Vector3 pos = new Vector3(Mathf.Cos(timeCounter) * distance, 0, Mathf.Sin(timeCounter)* distance);
+        Vector3 pos = new Vector3(Mathf.Cos(timeCounter) * distance, 0, Mathf.Sin(timeCounter) * distance);
         transform.position = pos;
+    }
+
+    void Revolution()
+    {
+        float x = Mathf.Cos(timeCounter * speed) * distance;
+        float z = Mathf.Sin(timeCounter * speed) * distance;
+
+        Vector3 pos = new Vector3(x, 0, z);
+
+        transform.position = pos;
+
     }
 }
